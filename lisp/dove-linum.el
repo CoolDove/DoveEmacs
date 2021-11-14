49bccfd9ec01031d798f02e098bb9a94e15d0e2e
CoolDove <1515098414@qq.com>
2021-11-14 19:38:07 +0000

    supper badass dove line number for evil

--- lisp/dove-linum.el
+++ lisp/dove-linum.el
@@ -8,24 +8,53 @@
   "linum highlight face"
   :group 'char-face
 )
+(defface linum-highlight-evil-register
+  '((t (:background "blue" :foreground "black")))
+  "linum highlight face"
+  :group 'char-face
+)
 
 (defun dove-evil-mode-string ()
-  (cond ((evil-insert-state-p)  "@")
-		((evil-visual-state-p)  ">")
+  (cond ((evil-insert-state-p)  ":")
+		((evil-visual-state-p)  "~")
 		((evil-replace-state-p) "=")
-		((evil-normal-state-p)  "~")
+		((evil-normal-state-p)  ">")
 		(t " "))
+)
+
+(defun dove-evil-register-string ()
+  (let ((num-len (length (number-to-string (line-number-at-pos (point-max)))))
+		(pre-space (make-string (- (length (number-to-string (line-number-at-pos (point-max)))) 1) 32)))
+	(if evil-this-register
+	    (concat pre-space (format "#%s" (char-to-string evil-this-register)))
+		"-")
+	)
+)
+
+;; finish this
+(defun dove-line-evil-mark (lnum)
+  "get the line mark"
 )
 
 (defun dove-linum (line-number)
   (let ((distance (- line-number dove-current-line))
-		(df-string (format "%%%ds" (+ (length (number-to-string (line-number-at-pos (point-max)))) 1))))
+		(df-string (format "%%%ds" (length (number-to-string (line-number-at-pos (point-max)))))))
 	(if (eql distance 0)
 		;; current line
-		(propertize
-		    (format (concat df-string (dove-evil-mode-string)) (number-to-string dove-current-line))
-		    'face (if (evil-insert-state-p) 'linum-highlight-insert
-					                        'linum-highlight))
+		(cond (evil-this-register ;; register mode
+			    (propertize
+					(dove-evil-register-string)
+					'face 'linum-highlight-evil-register))
+
+              (t                  ;; default, current line number
+			    (propertize
+					(format (concat df-string (dove-evil-mode-string)) (number-to-string dove-current-line))
+					'face (if (evil-insert-state-p)
+								'linum-highlight-insert
+								'linum-highlight)))
+
+		)
+		
 	    ;; other lines
 		(propertize
 		    (format (concat df-string " ") (number-to-string (cond ((< distance 0) (* -1 distance))
@@ -41,14 +70,13 @@
 	  (global-display-line-numbers-mode t))
     ;; terminal mode
 	(defvar dove-current-line 0)
-	(defvar dove-line-number-length 0)
 
     (setq linum-format 'dove-linum)
 	(global-linum-mode t)
 	(set-face-foreground 'linum "green")
 
 	(defadvice linum-update (around my-linum-update)
-	(let ((dove-current-line (line-number-at-pos)))
+	  (let ((dove-current-line (line-number-at-pos)))
 		ad-do-it))
 	(ad-activate 'linum-update)
 )
