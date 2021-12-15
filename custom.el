6ef20f5cc1505b106ad99cc26158e4bd5223c300
Doooooove <1515098414@qq.com>
2021-12-15 15:04:46 +0000

    further simplized

--- custom.el
+++ custom.el
@@ -10,14 +10,16 @@
  '(awesome-tray-mode-line-active-color "#0031a9")
  '(awesome-tray-mode-line-inactive-color "#d7d7d7")
  '(blink-cursor-mode nil)
- '(custom-enabled-themes '(misterioso))
+ '(custom-enabled-themes '(wheatgrass))
  '(custom-safe-themes
    '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "cf9414f229f6df728eb2a5a9420d760673cca404fee9910551caf9c91cff3bfa" "dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "1df2d767cc1b5ed78626f93f06c24ac15144a28b7420364769bf63cd23e420d3" "6bdcff29f32f85a2d99f48377d6bfa362768e86189656f63adbf715ac5c1340b" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" default))
  '(display-line-numbers-type 'relative)
+ '(display-time-mode t)
  '(fci-rule-color "#FFFFF8")
  '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-theme-fringe-red))
  '(flymake-note-bitmap '(exclamation-mark modus-theme-fringe-cyan))
  '(flymake-warning-bitmap '(exclamation-mark modus-theme-fringe-yellow))
+ '(fringe-mode 0 nil (fringe))
  '(highlight-tail-colors '(("#aecf90" . 0) ("#c0efff" . 20)))
  '(hl-sexp-background-color "#efebe9")
  '(hl-todo-keyword-faces
@@ -43,11 +45,15 @@
  '(ibuffer-filter-group-name-face 'modus-theme-mark-symbol)
  '(ibuffer-marked-face 'modus-theme-mark-sel)
  '(ibuffer-title-face 'modus-theme-pseudo-header)
+ '(indicate-buffer-boundaries '((t . right) (top . left)))
+ '(line-number-mode nil)
  '(package-selected-packages
    '(csharp-mode org-bullets heaven-and-hell darkroom pdf-tools dim eldoc-box dimmer diminish auto-dim-other-buffers mosey find-file-rg web-search quick-peek zoom-window on-screen elscreen centaur-tabs magit evil-ex-shell-command evil-visualstar goto-last-change undo-tree counsel shader-mode hlsl-mode eglot glsl-mode deadgrep evil-mc evil-anzu dove-linum pyim youdao-dictionary use-package smooth-scroll smart-mode-line restart-emacs projectile poly-markdown omnisharp ivy-posframe indent-guide gruvbox-theme evil-surround company all-the-icons))
  '(safe-local-variable-values '((eval when (fboundp 'rainbow-mode) (rainbow-mode 1))))
+ '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
+ '(tooltip-mode nil)
  '(vc-annotate-background "#FFFFFD")
  '(vc-annotate-background-mode nil)
  '(vc-annotate-color-map
@@ -70,6 +76,7 @@
 	 (340 . "#1B1809")
 	 (360 . "#1B1809")))
  '(vc-annotate-very-old-color "#4E4B3D")
+ '(window-divider-default-places 'bottom-only)
  '(xterm-color-names
    ["#000000" "#a60000" "#005e00" "#813e00" "#0031a9" "#721045" "#00538b" "#f0f0f0"])
  '(xterm-color-names-bright
@@ -79,4 +86,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Cascadia Code" :foundry "outline" :slant normal :weight normal :height 102 :width normal)))))
+ '(default ((t (:family "Cascadia Mono" :foundry "outline" :slant normal :weight light :height 120 :width normal)))))
--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -6,72 +6,9 @@
   (font-lock-add-keywords nil '(("\\(@[A-Za-z][A-Za-z]*:\\)"  1 '(:background "#e66735" :foreground "#2d3743" :box t) t)))
 )
 
-(defun dove-note-highlight-terminal ()
-  "set dove note highlight for current buffer"
-  (font-lock-add-keywords nil '(("\\<\\(FIXME:\\)" 1 '(:background "red"      :box t) t)))
-  (font-lock-add-keywords nil '(("\\<\\(NOTE:\\)"  1 '(:foreground "lightred" :box t) t)))
-  (font-lock-add-keywords nil '(("\\<\\(TODO:\\)"  1 '(:foreground "blue"     :box t) t)))
-  (font-lock-add-keywords nil '(("\\(@[A-Za-z][A-Za-z]*:\\)"  1 '(:background "yellow" :foreground "black" :box t) t)))
-)
-
 (defun dove-company-highlight-terminal ()
   "adjust highlight for company mode in terminal"
   (set-face-background 'company-tooltip "darkgray")
-)
-
-(defun dove-sml-highlight-terminal ()
-  "adjust highlight for sml in terminal"
-    (set-face-foreground 'sml/git                       "cyan")
-	(set-face-foreground 'sml/client                    "gray")
-	(set-face-foreground 'sml/col-number                "gray")
-	(set-face-foreground 'sml/filename                  "gray")
-	(set-face-foreground 'sml/folder                    "gray")
-	(set-face-foreground 'sml/global                    "gray")
-	(set-face-foreground 'sml/minor-modes               "gray")
-	(set-face-foreground 'sml/mule-info                 "gray")
-	(set-face-foreground 'sml/outside-modified          "gray")
-	(set-face-foreground 'sml/not-modified              "gray")
-	(set-face-foreground 'sml/remote                    "gray")
-	(set-face-foreground 'sml/sudo                      "gray")
-	(set-face-foreground 'sml/line-number               "gray")
-	(set-face-foreground 'sml/read-only                 "cyan")
-)
-
-(defun dove-highlight-text-terminal ()
-    (set-face-foreground font-lock-variable-name-face   "lightgray")
-    (set-face-foreground font-lock-string-face          "lightcyan")
-    (set-face-foreground font-lock-keyword-face         "green")
-    (set-face-foreground font-lock-function-name-face   "cyan")
-    (set-face-foreground font-lock-comment-face         "darkgray")
-    (set-face-foreground font-lock-type-face            "yellow")
-    (set-face-foreground 'button                        "lightgreen")
-
-    (set-face-foreground 'cursor                        "lightgray")
-    (set-face-background 'region                        "green")
-    (set-face-foreground 'region                        "black")
-    (set-face-foreground 'line-number                   "green")
-
-    (set-face-background 'isearch                       "lightblue")
-	(set-face-background 'show-paren-match              "darkgray")
-	(set-face-foreground 'show-paren-match              "lightgray")
-
-    (set-face-background 'tty-menu-disabled-face        "darkgray")
-    (set-face-foreground 'tty-menu-disabled-face        "yellow")
-    (set-face-background 'tty-menu-enabled-face         "darkgray")
-    (set-face-foreground 'tty-menu-enabled-face         "lightgray")
-    (set-face-background 'tty-menu-selected-face        "green")
-
-    (set-face-background 'header-line                   "white")
-
-    (set-face-background 'mode-line                     "white")
-    (set-face-foreground 'mode-line                     "yellow")
-    (set-face-background 'mode-line-inactive            "black")
-    (set-face-foreground 'mode-line-inactive            "lightgray")
-
-	(dove-sml-highlight-terminal)
-	(add-hook 'flycheck-mode '(lambda ((set-face-foreground 'flycheck-error "red"))))
-	(add-hook 'company-mode-hook 'dove-company-highlight-terminal)
-	(add-hook 'prog-mode-hook 'dove-note-highlight-terminal)
 )
 
 (if (display-graphic-p)
@@ -79,14 +16,15 @@
 	(progn
 		(add-hook 'prog-mode-hook 'dove-note-highlight)
 		(evil-set-cursor-color "green")
+        (set-face-foreground font-lock-comment-face "#4f6534")
 		(set-face-background 'region "#5daf68")
-		(set-face-foreground 'region "#2d3743")
+		(set-face-foreground 'region "#efefdd")
 	    (set-face-background 'show-paren-match "#00000000")
 	    (set-face-attribute 'show-paren-match nil :underline t)
 	    (set-face-foreground 'show-paren-match "#00ea66")
 	)
 	; Terminal highlight
-    (dove-highlight-text-terminal)
+    ;; (dove-highlight-text-terminal)
 )
 
 (provide 'init-highlight)
