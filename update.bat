525abde8697dc38711b3c755e8eb73693849c5f4
CoolDove <1515098414@qq.com>
2021-12-23 01:30:48 +0800

    update 2021-12-23[ 1:30] hello

--- update.bat
+++ update.bat
@@ -2,7 +2,7 @@
 
 git status -s
 git add .
-::echo %date%[%time%]
-git commit -m "update %date%[%time%]"
+echo %date:~0,10% [%time:~1,7%] %2
+::git commit -m "update %date:~0,4%[%time%]"
 ::git push hub master
 ::git push gitee master
