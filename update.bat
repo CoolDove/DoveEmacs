b49395f3b7defcf97dc678a514151818c50af124
CoolDove <1515098414@qq.com>
2021-12-23 01:32:58 +0800

    update 2021-12-23[ 1:32] polish

--- update.bat
+++ update.bat
@@ -1,8 +1,5 @@
 @echo off
-
 git status -s
-git add .
-echo %date:~0,10% [%time:~1,7%] %2
-::git commit -m "update %date:~0,4%[%time%]"
-::git push hub master
-::git push gitee master
+git commit -m "update %date:~0,10%[%time:~0, 5%] %1"
+git push hub master
+git push gitee master
