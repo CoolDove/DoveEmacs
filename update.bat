41c607abf5486b27f8226ddfd80554ee72c4cbdf
CoolDove <1515098414@qq.com>
2021-12-23 01:23:18 +0800

    update 2021-12-23 周四[ 1:23:18.70]

--- update.bat
+++ update.bat
@@ -1,5 +1,8 @@
+@echo off
+
 git status -s
 git add .
-git commit
-git push hub master
-git push gitee master
+::echo %date%[%time%]
+git commit -m "update %date%[%time%]"
+::git push hub master
+::git push gitee master
