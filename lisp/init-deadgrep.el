05c4a855295dd0eea7bacf1952a05d99af9a7220
Dove <1515098414@qq.com>
2022-01-14 10:16:57 +0800

    update

--- lisp/init-deadgrep.el
+++ lisp/init-deadgrep.el
@@ -6,9 +6,6 @@
   :defer nil
   :config (with-eval-after-load 'evil
 			(evil-set-initial-state 'deadgrep-mode 'emacs))
-            (defun dove/deadgreap-call ()
-	            (interactive)
-	            (message "call deadgrep"))
 )
 
 (provide 'init-deadgrep)
