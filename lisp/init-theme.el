b3cb0bfd2ea3a92259de4f43cd561c4f2454fb4c
Dove <1515098414@qq.com>
2022-09-16 00:20:31 +0800

    update themes
    * lisp/init-theme.el:

---
+++ lisp/init-theme.el
@@ -0,0 +1,13 @@
+(load-theme 'whiteboard)
+(set-background-color "#efefea")
+(set-face-background 'region "yellowgreen")
+(evil-set-cursor-color "darkgreen")
+
+(set-face-attribute 'line-number-current-line nil :bold t)
+(set-face-attribute 'line-number-current-line nil :underline t)
+
+(set-face-background 'line-number "#e0e2e0")
+(set-face-background 'line-number-current-line "#efbb23")
+(set-face-foreground 'line-number-current-line "#1133ef")
+
+(provide 'init-theme)
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -2,43 +2,29 @@
 (scroll-bar-mode -1)
 (menu-bar-mode   -1)
 
-;; Chinese font in GUI mode
-;; (when (display-graphic-p)
-	;; (dolist (charset '(kana han symbol cjk-misc bopomofo))
-        ;; (set-fontset-font (frame-parameter nil 'font)
-						  ;; charset (font-spec :family "Microsoft Yahei Mono" :size 16))))
-
+(require 'init-theme)
 
 (setq display-line-numbers-type 'relative)
 ;; (add-hook 'prog-mode-hook #'display-line-numbers-mode 1)
 
-;; (use-package railscasts-theme
-  ;; :ensure t
-  ;; :init (load-theme 'railscasts))
-
-
-;; (use-package darcula-theme
-  ;; :ensure t
-  ;; :init (load-theme 'darcula))
-(load-theme 'deeper-blue)
-
 (use-package smart-mode-line
   :ensure t)
 
+(set-face-background 'mode-line "yellowgreen")
 (set-face-background 'mode-line-inactive "system background")
-(set-face-foreground 'mode-line-inactive "white")
-(set-face-attribute 'mode-line-inactive nil :box nil)
-(set-face-attribute 'mode-line-inactive nil :underline t)
+(set-face-foreground 'mode-line-inactive "darkblue")
+(set-face-attribute  'mode-line-inactive nil :box t)
+(set-face-attribute  'mode-line-inactive nil :underline t)
  
-(setq frame-title-format (concat "emacs is shit!!!" (format-time-string "    [%Y-%m-%d, %a]")))
+(setq frame-title-format "emacs is shit")
 
 (defun dove/set-title (name)
   (interactive "sSet new title: ")
   "set the stupid emacs title"
   (setq frame-title-format name))
+
 (with-eval-after-load 'evil
   (evil-define-key '(normal visual motion operator) 'global (kbd "<leader>t") #'dove/set-title))
-
 
 ;; (use-package dimmer
   ;; :ensure t
@@ -59,7 +45,7 @@
 	(set-face-attribute 'markdown-table-face "Unifont")
 )
 
-;; (require 'zone)
-;; (zone-when-idle 180)
+(require 'zone)
+(zone-when-idle 180)
 
 (provide 'init-ui)
